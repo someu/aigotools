03080011e6444ee2bd1608a73e072e6490311328
someu <someuxyz@gmail.com>
2024-06-13 23:33:10 +0800

    feat: change name

--- packages/crawler/src/app.controller.ts
+++ packages/crawler/src/app.controller.ts
@@ -1,17 +1,7 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Logger,
-  Param,
-  Post,
-  Res,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppService, BatchParams } from './app.service';
 import { MinioService } from './providers/minio.service';
-import { Response } from 'express';
-import { error } from 'console';
 
 @Controller()
 export class AppController {
@@ -31,23 +21,5 @@ export class AppController {
   async batchStopSites(@Body() body: BatchParams) {
     const count = await this.appService.batchStopSiteCrawl(body);
     return count;
-  }
-
-  @Get('image/:file')
-  async getImage(@Param('file') file: string, @Res() res: Response) {
-    const { stat, stream } = await this.minioService.getFileStream(file);
-    stream.on('data', (chunk) => {
-      res.write(chunk, 'binary');
-    });
-    res.set({
-      'Content-Type': stat.metaData['content-type'],
-    });
-    stream.on('end', () => {
-      res.end();
-    });
-    stream.on('error', () => {
-      Logger.error(`Get file ${file} error: ${error}`);
-      throw new Error('Get file error');
-    });
   }
 }
