903884e3e971d394913d266e103aa2982595581a
someu <someuxyz@gmail.com>
2024-06-17 17:55:05 +0800

    feat: sitemap

---
+++ packages/aigotools/src/app/sitemap.ts
@@ -0,0 +1,113 @@
+import path from "path";
+
+import { MetadataRoute } from "next";
+
+import dbConnect from "@/lib/db-connect";
+import { SiteModel } from "@/models/site";
+import { SiteState } from "@/lib/constants";
+import { AvailableLocales } from "@/lib/locales";
+import { AppConfig } from "@/lib/config";
+
+const perSitemapCount = 1000;
+
+export async function generateSitemaps() {
+  await dbConnect();
+
+  const siteCount = await SiteModel.countDocuments({
+    state: SiteState.published,
+  });
+
+  const siteMapCount = Math.ceil(siteCount / perSitemapCount);
+
+  const siteMapIds = new Array(siteMapCount).fill(0).map((_, id) => {
+    return {
+      id,
+    };
+  });
+
+  return [{ id: -1 }, ...siteMapIds];
+}
+
+export default async function sitemap({ id }: { id: number }) {
+  const sitemapRoutes: MetadataRoute.Sitemap = [];
+
+  if (id === -1) {
+    // base page sitemap
+    sitemapRoutes.push(
+      {
+        url: "",
+        lastModified: new Date(),
+        changeFrequency: "hourly",
+        priority: 1,
+      },
+      {
+        url: "categories",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+      {
+        url: "search",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      },
+      {
+        url: "submit",
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: 0.5,
+      }
+    );
+  } else {
+    // sites page site map
+    await dbConnect();
+
+    const siteKeyObjs = await SiteModel.aggregate([
+      {
+        $match: {
+          state: SiteState.published,
+        },
+      },
+      {
+        $sort: {
+          createdAt: 1,
+        },
+      },
+      {
+        $skip: id * perSitemapCount,
+      },
+      {
+        $limit: (id + 1) * perSitemapCount,
+      },
+      {
+        $project: {
+          _id: 0,
+          siteKey: 1,
+        },
+      },
+    ]);
+
+    sitemapRoutes.push(
+      ...siteKeyObjs.map(({ siteKey }) => {
+        return {
+          url: `/s/${siteKey}`,
+          lastModified: new Date(),
+          changeFrequency: "monthly" as const,
+          priority: 0.9,
+        };
+      })
+    );
+  }
+
+  const sitemapData = sitemapRoutes.flatMap((route) =>
+    AvailableLocales.map((locale) => {
+      return {
+        ...route,
+        url: path.join(AppConfig.siteUrl, locale, route.url),
+      };
+    })
+  );
+
+  return sitemapData;
+}
